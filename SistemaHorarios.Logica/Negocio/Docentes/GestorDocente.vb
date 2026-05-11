4fd8e9a9a541592945c6c4c4aa6e5bcb43676f17
Sebastian Villaneda <sebastianvillaneda202@gmail.com>
2026-05-10 21:57:54 -0500

    feat: se implementa crud del docente

---
+++ SistemaHorarios.Logica/Negocio/Docentes/GestorDocente.vb
@@ -0,0 +1,3 @@
+﻿Public Class GestorDocente
+
+End Class
--- SistemaHorarios.Modelos/Modelos/Docente.vb
+++ SistemaHorarios.Modelos/Modelos/Docente.vb
@@ -1,3 +1,17 @@
-﻿Public Class Docente
-    'Atributos
-End Class
+﻿Namespace Modelos
+
+    Public Class Docente
+
+        Public Property Id As Integer
+
+        Public Property Identificacion As String
+
+        Public Property Nombre As String
+
+        Public Property CorreoElectronico As String
+
+        Public Property Estado As Boolean
+
+    End Class
+
+End Namespace
